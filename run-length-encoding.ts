c2833b88803457bc323c08d8e0f41d6cb8718746
Lucas Oliveira <hub.lucasoliveira@gmail.com>
2020-09-01 19:05:26 -0300

    Refactory RunLengthEncoding class

--- run-length-encoding.ts
+++ run-length-encoding.ts
@@ -1,14 +1,14 @@
 class RunLengthEncoding {
   static encode(plaintext: string) {
-    return plaintext.replace(/(.)\1+/g, (match: string, character: string) => {
-      return `${match.length}${character}`
-    })
+    return plaintext.replace(/(.)\1+/g, (match: string, character: string) =>
+      `${match.length}${character}`
+    )
   }
 
   static decode(encodedText: string) {
-    return encodedText.replace(/(\d+)(\D)/g, (_, times: string, character: string) => {
-      return character.repeat(Number(times))
-    })
+    return encodedText.replace(/(\d+)(\D)/g, (_, times: string, character: string) =>
+      character.repeat(Number(times))
+    )
   }
 }
 
